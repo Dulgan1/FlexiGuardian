b4ce7cc197ce64f651901f44483dc92255b4297b
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-21 13:25:01 +0100

    Init files and commit

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Defines Base Model for all models of FlexiG project"""
+from datetime import datetime
+import models
+from sqlalchemy import Column, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+from uuid
+
+Base = declarative_base()
+
+
+class BaseModel:
+    id = Column(String(60), primary_key=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow)
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    setattr(self, k, v)
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        "Returns string representation of BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
