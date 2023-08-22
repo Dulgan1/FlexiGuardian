d5c186bd8bc2983e40f26d2a52f40b67627bdd83
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-22 14:11:33 +0100

    Fix Typo

--- models/contract.py
+++ models/contract.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Contract Class defined """
-import models.base_model import BaseModel, Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
