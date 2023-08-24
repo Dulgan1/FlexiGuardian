265aff38852b5fb1bcdc2b09766f9f130016e854
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-24 14:55:23 +0100

    Fixing

--- models/business.py
+++ models/business.py
@@ -4,7 +4,7 @@ from sqlalchemy import Column, String, ForeignKey
 class Business(BaseModel, Base):
     __tablename__ = "businesses"
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    address = Column(String(60), nullable=False)
+    address = relationship('Address', backref='business')
     description = Column(String(5000), nullable=False)
     image_url = Column(String(500), nullable=True)
 
