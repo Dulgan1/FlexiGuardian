71c13e087d4b0046c9ee656699210caf13e668be
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-27 15:56:54 +0100

    Syntax fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from flask import Blueprint
+from models import storage
 
 api_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
 
@@ -7,7 +8,10 @@ from api.v1.views.user import *
 #from api.v1.views.contract import *
 from api.v1.views.token import *
 
-def calc_tot_rate(user_id):                                                                 user = storage.get(User, user_id)                                                       _session = storage.session()                                                            reviews = _session.query(Review).filter(Review.for_user_id==user_id)
+def calc_tot_rate(user_id):
+    user = storage.get(User, user_id)
+    _session = storage.session()
+    reviews = _session.query(Review).filter(Review.for_user_id==user_id)
     total = 0
     count = 0
     for review in reviews:
