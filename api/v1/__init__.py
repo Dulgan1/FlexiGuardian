44f849b986c84b3ae6a70e62c25c1292c8cc064c
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-08-30 01:30:31 +0100

    Finc fix"

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,3 +1,12 @@
-def calc_tot_rate(user_id):                                                       user = storage.get(User, user_id)                                             _session = storage.session()                                                  reviews = _session.query(Review).filter(Review.for_user_id==user_id)          total = 0                                                                     count = 0                                                                     for review in reviews:
-        total += review.rating                                                        count += 1
-    rate_tot = total / count                                                                                                                                    user.rating = rate_tot                                                        storage.save
+def calc_tot_rate(user_id):
+    user = storage.get(User, user_id)
+    _session = storage.session()
+    reviews = _session.query(Review).filter(Review.for_user_id==user_id)
+    total = 0
+    count = 0
+    for review in reviews:
+        total += review.rating
+        count += 1
+    rate_tot = total / count
+    user.rating = rate_tot
+    storage.save
