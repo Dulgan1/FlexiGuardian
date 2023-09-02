1cdbcb530e1ab0ba9edbab6bd3b625c58f29cd4e
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-09-02 12:11:24 +0100

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #!usr/bin/python3
 """  FlexiGuardian Main App"""
-from flask import Flask
+from flask import Flask, render_template
+from flask_cors import CORS
 from api.v1.views import api_views
 from app_dynamics import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
